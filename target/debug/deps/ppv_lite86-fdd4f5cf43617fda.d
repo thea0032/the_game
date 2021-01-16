03e5ccf3b8198d14055889d5e72cd605785a0b1c
theachan0032 <hilttheo0032@apps.hopkinsschools.org>
2021-01-16 03:59:35 -0600

    removed this stupid stuff

--- target/debug/deps/ppv_lite86-fdd4f5cf43617fda.d
+++
@@ -1,9 +0,0 @@
-c:\Users\ilike\Projects\Rust\game_2\target\debug\deps\ppv_lite86-fdd4f5cf43617fda.rmeta: C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\lib.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\soft.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\types.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\x86_64\mod.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\x86_64\sse2.rs
-
-c:\Users\ilike\Projects\Rust\game_2\target\debug\deps\ppv_lite86-fdd4f5cf43617fda.d: C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\lib.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\soft.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\types.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\x86_64\mod.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\x86_64\sse2.rs
-
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\lib.rs:
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\soft.rs:
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\types.rs:
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\x86_64\mod.rs:
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\ppv-lite86-0.2.10\src\x86_64\sse2.rs:
--- target/debug/deps/rand_chacha-a7ae5eeaf0ee7224.d
+++
@@ -1,7 +0,0 @@
-c:\Users\ilike\Projects\Rust\game_2\target\debug\deps\rand_chacha-a7ae5eeaf0ee7224.rmeta: C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs
-
-c:\Users\ilike\Projects\Rust\game_2\target\debug\deps\rand_chacha-a7ae5eeaf0ee7224.d: C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs
-
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\lib.rs:
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\chacha.rs:
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\rand_chacha-0.3.0\src\guts.rs:
