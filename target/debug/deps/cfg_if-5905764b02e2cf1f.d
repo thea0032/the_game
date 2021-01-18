47fd872f58dfa758a04ce367ca1441b26bdf550d
theachan0032 <hilttheo0032@apps.hopkinsschools.org>
2021-01-18 17:24:43 -0600

    deleted target folder

--- target/debug/deps/cfg_if-5905764b02e2cf1f.d
+++
@@ -1,5 +0,0 @@
-c:\Users\ilike\Projects\Rust\game_2\target\debug\deps\cfg_if-5905764b02e2cf1f.rmeta: C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-c:\Users\ilike\Projects\Rust\game_2\target\debug\deps\cfg_if-5905764b02e2cf1f.d: C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs
-
-C:\Users\ilike\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-0.1.10\src\lib.rs:
