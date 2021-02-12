e85d385d0f2c892e76cc4f98d331814eafd3344f
theachan0032 <hilttheo0032@apps.hopkinsschools.org>
2021-02-12 17:42:08 -0600

    removed debug stuff

--- src/object/template.rs
+++ src/object/template.rs
@@ -1,5 +1,3 @@
-use std::io::stdin;
-
 use crate::{resources::ResourceDict, systems::{object_id::ObjectID, Systems}};
 
 #[derive(Clone, Debug)]
@@ -22,8 +20,6 @@ impl Template {
         &self.surplus
     }
     pub fn install(&self, obj: ObjectID, sys: &mut Systems) -> bool {
-        println!("Installing {:?}", self);
-        stdin().read_line(&mut String::new());
         if sys.get_o(obj).resources_mut().spend(self.cost()) {
             sys.get_o(obj).resources_mut().add_storage_vec(self.storage());
             sys.get_o(obj).resources_mut().add_surplus_vec(self.surplus());
